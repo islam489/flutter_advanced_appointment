71417d3be8bb15254fe6c56aee9cb536bc160be3
Islam Mashaal <63744359+islam489@users.noreply.github.com>
2023-11-28 15:19:36 +0200

    added splash screen

--- lib/features/login/ui/screen/loginScreen.dart
+++ lib/features/login/ui/screen/loginScreen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_appointment/core/Theming/styles.dart';
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('loginScreen'),
+    return  Scaffold(
+      body: Center(
+        child: Text('Login Screen',
+          textAlign: TextAlign.center,
+          style: TextStyles.font32BlueBold)
+      ),
     );
   }
 }
--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -1,9 +1,7 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_advanced_appointment/core/Helpers/extensions.dart';
 import 'package:flutter_advanced_appointment/features/onBoarding/ui/screen/onboardingScreen.dart';
 
-import '../../core/Routing/routes.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
@@ -17,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return  AnimatedSplashScreen(
       splashIconSize: 800.0,
-      duration: 2500,
+      duration: 1000,
         splash: 'assets/images/splash.png',
         nextScreen: const OnBoardingScreen(),
       splashTransition: SplashTransition.slideTransition,
