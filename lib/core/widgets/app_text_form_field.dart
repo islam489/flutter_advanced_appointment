4e7e397909ad591781df77e84d3fc63e66e85f8d
Islam Mashaal <63744359+islam489@users.noreply.github.com>
2024-02-12 10:51:40 +0200

    add flutter flavors

--- lib/core/widgets/app_text_form_field.dart
+++ lib/core/widgets/app_text_form_field.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../Theming/colors.dart';
 import '../Theming/styles.dart';
 
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_appointment/core/Routing/app_router.dart';
 import 'package:flutter_advanced_appointment/doc_app.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'core/DI/dependency_injection.dart';
 
-void main() {
+void main() async{
   setupGetIt();
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
 }
+
